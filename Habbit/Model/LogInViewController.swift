5e93bf0ee97b819e8cf1370c5ba41955a8781015
Brandon David <brandonhudavid@berkeley.edu>
2018-04-15 12:48:21 -0700

    authentication

--- Habbit/Model/LogInViewController.swift
+++ Habbit/Model/LogInViewController.swift
@@ -7,3 +7,73 @@
 //
 
 import Foundation
+import UIKit
+import FirebaseAuth
+
+class LogInViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var userEmail = ""
+    var userPassword = ""
+    
+    @IBAction func logInPressed(_ sender: UIButton) {
+        guard let emailText = emailTextField.text else { return }
+        guard let passwordText = passwordTextField.text else { return }
+        
+        if emailText == "" || passwordText == "" {
+            let alertController = UIAlertController(title: "Log In Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else {
+            Auth.auth().signIn(withEmail: emailText, password: passwordText) { (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "logInToMainPage", sender: nil)
+                } else {
+                    let alertController = UIAlertController(title: "Log In Error", message:
+                        error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    
+    @IBAction func signUpPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "logInToSignUp", sender: self)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        Auth.auth().addStateDidChangeListener() { (auth, user) in
+            if let user = user {
+                self.performSegue(withIdentifier: "logInToMainPage", sender: nil)
+            }
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == self.emailTextField {
+            if textField.text != nil {
+                self.userEmail = textField.text!
+            }
+        } else {
+            if textField.text != nil {
+                self.userPassword = textField.text!
+            }
+        }
+    }
+}
+
--- Habbit/Model/SignUpViewController.swift
+++ Habbit/Model/SignUpViewController.swift
@@ -7,3 +7,98 @@
 //
 
 import Foundation
+import UIKit
+import FirebaseAuth
+
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordVerificationTextField: UITextField!
+    
+    var userEmail = ""
+    var userName = ""
+    var userPassword = ""
+    var userVerifiedPassWord = ""
+    
+    @IBAction func signUpPressed(_ sender: UIButton) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let name = nameTextField.text else { return }
+        guard let verifiedPassword = passwordVerificationTextField.text else { return }
+        if email == "" || password == "" || name == "" || verifiedPassword == "" {
+            let alertController = UIAlertController(title: "Form Error.", message: "Please fill in form completely.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        } else {
+            Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                if error == nil {
+                    let changeRequest = user!.createProfileChangeRequest()
+                    changeRequest.displayName = name
+                    changeRequest.commitChanges() { (error) in
+                        if let error = error {
+                            let alertController = UIAlertController(title: "Sign Up Error", message:
+                                error.localizedDescription, preferredStyle: .alert)
+                            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                            alertController.addAction(defaultAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                    }
+                    let alertController = UIAlertController(title: "Signed Up", message: "Registration successful!", preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: { action in
+                        self.performSegue(withIdentifier: "signUpToMainPage", sender: nil) })
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                    
+                } else if password != verifiedPassword {
+                    let alertController = UIAlertController(title: "Verification Error.", message: "The two passwords do not match.", preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.passwordVerificationTextField.textColor = UIColor.red
+                    self.present(alertController, animated: true, completion: nil)
+                } else {
+                    let alertController = UIAlertController(title: "Sign Up Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func logInPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "signUpToLogIn", sender: self)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.nameTextField.delegate = self
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        self.passwordVerificationTextField.delegate = self
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == self.emailTextField {
+            if textField.text != nil {
+                self.userEmail = textField.text!
+            }
+        } else if textField == self.passwordTextField {
+            if textField.text != nil {
+                self.userPassword = textField.text!
+            }
+        } else if textField == self.nameTextField {
+            if textField.text != nil {
+                self.userName = textField.text!
+            }
+        } else if textField == self.passwordVerificationTextField {
+            if textField.text != nil {
+                self.userVerifiedPassWord = textField.text!
+            }
+        }
+    }
+}
