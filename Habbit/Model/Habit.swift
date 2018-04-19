74371f4bb6e74956b721af981c89241035b60f59
Brandon David <brandonhudavid@berkeley.edu>
2018-04-19 00:09:13 -0700

    configure firebase test

--- Habbit/Model/Habit.swift
+++ Habbit/Model/Habit.swift
@@ -11,14 +11,14 @@ import UIKit
 
 class Habit {
     let habitName: String
-    let habitIcon: UIImage
-    let daysPerformed: [String]
-    let color: String
+    let habitIconPath: String
+    let habitDays: [String]
+    let habitColor: String
     
-    init(habitName: String, habitIcon: UIImage, daysPerformed: [String], color: String) {
+    init(habitName: String, habitIconPath: String, habitDays: [String], habitColor: String) {
         self.habitName = habitName
-        self.habitIcon = habitIcon
-        self.daysPerformed = daysPerformed
-        self.color = color
+        self.habitIconPath = habitIconPath
+        self.habitDays = habitDays
+        self.habitColor = habitColor
     }
 }
